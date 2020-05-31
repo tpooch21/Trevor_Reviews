8fdb8d0f8d0e59e17ac27849bf6a1ccc74877453
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-30 20:43:20 -0700

    Delete irrelevant ds bulk loader commands

--- LegacyService/TrevorService/Trevor_Database/cassandraSeed.bash
+++ LegacyService/TrevorService/Trevor_Database/cassandraSeed.bash
@@ -1,89 +1,49 @@
 #!/bin/bash
-dsbulk load -url places.csv -k bnbreviews -t places -header true
+dsbulk load -url TrevorService/Trevor_Database/places.csv -k bnbreviews -t places -header true
 
-dsbulk load -url users.csv -k bnbreviews -t users -header true
+dsbulk load -url TrevorService/Trevor_Database/users.csv -k bnbreviews -t users -header true
 
-dsbulk load -url cassReviews/reviewsCass.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass2.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass2.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass3.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass3.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass4.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass4.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass5.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass5.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass6.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass6.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass7.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass7.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass8.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass8.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass9.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass9.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass10.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass10.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass11.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass11.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass12.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass12.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass13.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass13.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass14.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass14.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass15.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass15.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass16.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass16.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass17.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass17.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass18.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass18.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass19.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass19.csv -k bnbreviews -t reviews_by_place -header true
 
-dsbulk load -url cassReviews/reviewsCass20.csv -k bnbreviews -t reviews_by_place -header true
+dsbulk load -url TrevorService/Trevor_Database/cassReviews/reviewsCass20.csv -k bnbreviews -t reviews_by_place -header true
 
-#########
 
-# dsbulk load -url cassReviews/reviewsCass.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass2.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass3.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass4.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass5.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass6.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass7.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass8.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass9.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass10.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass11.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass12.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass13.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass14.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass15.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass16.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass17.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass18.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass19.csv -k bnbreviews -t reviews -header true
-
-# dsbulk load -url cassReviews/reviewsCass20.csv -k bnbreviews -t reviews -header true
 
 
 
