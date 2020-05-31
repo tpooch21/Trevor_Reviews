1f4a0dd851a05296279f11d0a9ec26e6c05f783c
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-30 20:42:07 -0700

    Define fetchReviews and writeReview model functions

--- LegacyService/TrevorService/Trevor_Database/models.js
+++ LegacyService/TrevorService/Trevor_Database/models.js
@@ -1,18 +1,41 @@
-const db = require('./psqlindex.js');
-
+const db = require('./connect.js');
+const fs = require('fs');
+const path = require('path');
 
 const fetchReviews = (placeID, callback) => {
-  const queryString = `SELECT * FROM reviews JOIN users ON reviews.place_id=${placeID} AND reviews.user_id = users.user_id`;
+  const queryString = `SELECT * FROM reviews_by_place WHERE place_id=${placeID}`;
 
-  db.query(queryString, (err, results) => {
+  db.client.execute(queryString, (err, results) => {
     if (err) {
       callback(err);
     } else {
-      callback(null, results.rows);
+      callback(null, results);
     }
   });
-}
+};
+
+const writeReview = (review, placeID, callback) => {
+
+  const reviewID = fs.readFileSync(path.join(__dirname + '/reviewIdTracker.txt'), 'utf-8');
+  const queryString = `INSERT INTO reviews_by_place (review_id, date_published, comment, checkin_rating, accuracy_rating, value_rating, communication_rating, cleanliness_rating, location_rating, user_id, place_id, first_name, last_name, avatar) VALUES (${reviewID}, '${review.date_published}', '${review.comment}', ${review.checkin_rating}, ${review.accuracy_rating}, ${review.value_rating}, ${review.communication_rating}, ${review.cleanliness_rating}, ${review.location_rating}, ${review.user_id}, ${placeID}, '${review.first_name}', '${review.last_name}', '${review.avatar}')`;
+
+  console.log('Logging queryString => ', queryString);
+  db.client.execute(queryString, (err, results) => {
+    if (err) {
+      callback(err);
+    }
+    callback(null, results);
+  });
+
+  var nextNumber = Number(reviewID) + 1;
+  fs.writeFile(path.join(__dirname + '/reviewIdTracker.txt'), nextNumber, (err) => {
+    if (err) {
+      console.log('Error writing data to file => ', err);
+    }
+  });
+};
 
 module.exports = {
-  fetchReviews
+  fetchReviews,
+  writeReview
 };
