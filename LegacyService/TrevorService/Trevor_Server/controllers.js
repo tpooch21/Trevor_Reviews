800cef76240e35c0920bc5398484f48c01328781
Trevor Puccini <trevorpuccini21@gmail.com>
2020-05-30 20:44:36 -0700

    Define controller functions and build out API to handle GET requests for all reviews associated with a particular place ID, and to handle POST requests for user reviews

--- LegacyService/TrevorService/Trevor_Server/controllers.js
+++ LegacyService/TrevorService/Trevor_Server/controllers.js
@@ -3,12 +3,26 @@ const models = require('../Trevor_Database/models.js');
 const getAllReviews = (req, res) => {
   models.fetchReviews(req.params.id, (err, results) => {
     if (err) {
+      console.log('Error fetching data => ', err);
       res.status(404).end();
+    } else {
+      res.status(200).json(results.rows);
     }
-    res.status(200).json(results);
+  });
+};
+
+const postReview = (req, res) => {
+  models.writeReview(req.body, req.params.id, (err, results) => {
+    if (err) {
+      console.log('Error posting data => ', err);
+      res.status(501).end();
+    } else {
+      res.status(201).end();
+    }
   });
 };
 
 module.exports = {
-  getAllReviews
+  getAllReviews,
+  postReview
 };
--- LegacyService/TrevorService/Trevor_Server/index.js
+++ LegacyService/TrevorService/Trevor_Server/index.js
@@ -1,5 +1,7 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const app = express();
+const path = require('path');
 const controllers = require('./controllers.js');
 
 const bodyParser = require('body-parser');
@@ -8,9 +10,19 @@ const port = 3000;
 
 app.use(morgan('tiny'));
 
+app.use(express.static(path.join(__dirname + '/../../public/')));
+
 app.use(bodyParser.json());
 
 app.get('/api/place/:id/reviews', controllers.getAllReviews);
+
+app.post('/api/place/:id/review', controllers.postReview);
+
+// app.patch('/api/:place_id/review/:review_id')
+
+// app.delete('/api/:place_id/review/:review_id')
+
+// app.delete('api/place/:id')
 
 app.listen(port, () => {console.log(`Server is listening at http://localhost:${port}`)});
 
